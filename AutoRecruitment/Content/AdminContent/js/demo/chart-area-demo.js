612a4e37deca26ea57acd81cda01ee712c1250e3
mshehab <mshehab@niti.edu.sa>
2021-09-16 15:47:10 +0300

    update UI

--- AutoRecruitment/Content/AdminContent/js/demo/chart-area-demo.js
+++ AutoRecruitment/Content/AdminContent/js/demo/chart-area-demo.js
@@ -32,9 +32,9 @@ var ctx = document.getElementById("myAreaChart");
 var myLineChart = new Chart(ctx, {
   type: 'line',
   data: {
-    labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+      labels: ["Camp 1", "Camp 2", "Camp 3", "Camp 4", "Camp 5", "Camp 6", "Camp 7", "Camp 8"],
     datasets: [{
-      label: "Earnings",
+      label: "Applicants",
       lineTension: 0.3,
       backgroundColor: "rgba(78, 115, 223, 0.05)",
       borderColor: "rgba(78, 115, 223, 1)",
@@ -46,7 +46,7 @@ var myLineChart = new Chart(ctx, {
       pointHoverBorderColor: "rgba(78, 115, 223, 1)",
       pointHitRadius: 10,
       pointBorderWidth: 2,
-      data: [0, 10000, 5000, 15000, 10000, 20000, 15000, 25000, 20000, 30000, 25000, 40000],
+      data: [4000, 6000, 8000, 10000, 9536, 7260, 5670, 6897],
     }],
   },
   options: {
@@ -78,7 +78,8 @@ var myLineChart = new Chart(ctx, {
           padding: 10,
           // Include a dollar sign in the ticks
           callback: function(value, index, values) {
-            return '$' + number_format(value);
+            //return '$' + number_format(value);
+            return number_format(value);
           }
         },
         gridLines: {
@@ -110,7 +111,8 @@ var myLineChart = new Chart(ctx, {
       callbacks: {
         label: function(tooltipItem, chart) {
           var datasetLabel = chart.datasets[tooltipItem.datasetIndex].label || '';
-          return datasetLabel + ': $' + number_format(tooltipItem.yLabel);
+          //return datasetLabel + ': $' + number_format(tooltipItem.yLabel);
+          return datasetLabel + ': ' + number_format(tooltipItem.yLabel);
         }
       }
     }
--- AutoRecruitment/Content/AdminContent/js/demo/chart-pie-demo.js
+++ AutoRecruitment/Content/AdminContent/js/demo/chart-pie-demo.js
@@ -7,7 +7,7 @@ var ctx = document.getElementById("myPieChart");
 var myPieChart = new Chart(ctx, {
   type: 'doughnut',
   data: {
-    labels: ["Direct", "Referral", "Social"],
+      labels: ["Qualification", "Rules Acceptance", "Interview"],
     datasets: [{
       data: [55, 30, 15],
       backgroundColor: ['#24405c', '#1cc88a', '#36b9cc'],
